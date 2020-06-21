84a4266cb523d6a96c4deb73a9decd9be9c8cf01
shaan360 <shaan@uexel.com>
2020-06-21 15:25:47 +0500

    initial commit

---
+++ Unifonic/API/Exception.php
@@ -0,0 +1,7 @@
+<?php
+namespace common\components\Unifonic\API;
+
+class Exception extends \Exception
+{
+
+}
---
+++ Unifonic/config.php
@@ -0,0 +1,7 @@
+<?php
+namespace common\components\Unifonic;
+
+return array(
+    'AppSid' => ['Your Rest AppSid'],
+    'ApiURL' => 'http://api.unifonic.com/rest/',
+);
